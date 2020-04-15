e071dbf81abd3daaac8a265c4a346c09f59d9463
aleflavia < Primary>
2020-04-15 19:33:08 +0200

    UI, reference 3D assets and reference animations for Station 4.
    Use only Main Scene. Test is only an animation test.
    In the main scene you can find the Main UI Canvas.
    Then I have listed all the following screens divided in Canvas (containing the UI elements) and Objects (3D assets, animations, etc)
    Reference animations: In Launch Sequence Objets(rotation of the sattelite) and in Filters-Canvas (animation of pointer lines)
    I have only one script for a slider that was very hard to setup (SliderClamper)

---
+++ Assets/SliderClamper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderClamper : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+    
+    [SerializeField] private float minClamp;
+    [SerializeField] private float maxClamp;
+
+    private void Start()
+    {
+        slider.value = minClamp;
+    }
+
+    public void ValueChange()
+    {
+        if (slider.value < minClamp)
+        {
+            slider.value = minClamp;
+        }
+
+        if (slider.value > maxClamp)
+        {
+            slider.value = maxClamp;
+        }
+    }
+
+
+    
+}
