c2bdce38b006abf9d99bf244c6ce30ccb0be36f6
Yanivg2906 <55092334+Yanivg2906@users.noreply.github.com>
2020-04-20 14:10:48 +0200

    added camera movement script
    will be added to buttons to ensure flow between screens

--- Assets/scripts/scene 4/screencontol.cs
+++ Assets/scripts/scene 4/screencontol.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class screencontol : MonoBehaviour
 {
+
+    private int CurrentScreen = 1;
+    private int NextDot;
+    public GameObject NextPosition;
+    private bool OnMove = false;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,27 @@ public class screencontol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if (OnMove)
+        {
+            MoveCamera();
+        }
+    }
+
+    public void NextButton()
+    {
+        CurrentScreen += 1;
+        string NextScreenName = "CheckPoint" + CurrentScreen.ToString();
+        NextPosition = GameObject.Find(NextScreenName);
+        OnMove = true;
+    }
+
+    void MoveCamera()
+    {
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, NextPosition.transform.position, 0.1f);
+        transform.position = SmoothedPosition;
+        if (transform.position==NextPosition.transform.position)
+        {
+            OnMove = false;
+        }
     }
 }
