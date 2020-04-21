f8fdc89a4a9ef3340090d583c5853b85b3cd2c49
carlesibanez <51021153+carlesibanez@users.noreply.github.com>
2020-04-21 22:58:03 +0200

    Satellites functionality

--- Assets/scripts/scene 4/ScreenControl.cs
+++ Assets/scripts/scene 4/ScreenControl.cs
@@ -12,6 +12,7 @@ public class ScreenControl : MonoBehaviour
     public GameObject NextPosition;
     public GameObject CurDot;
     private bool OnMove = false;
+    private bool OnPath = true;
     public Color C1;
     public Color C2;
    
@@ -47,6 +48,15 @@ public class ScreenControl : MonoBehaviour
     void SetTarget()
     {
         string NextScreenName = "CheckPoint" + CurrentScreen.ToString();
+        if (OnPath)
+        {
+            NextScreenName = "CheckPoint" + CurrentScreen.ToString();
+        }
+        else
+        {
+            NextScreenName = "CheckPoint" + CurrentScreen.ToString() + "f";
+        }
+
         NextPosition = GameObject.Find(NextScreenName);
         OnMove = true;
     }
