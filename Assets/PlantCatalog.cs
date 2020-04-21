d89d7915e0054714839ca446c7607c7d5e074610
Yanivg2906 <55092334+Yanivg2906@users.noreply.github.com>
2020-04-21 15:57:25 +0200

    added planet cataloge

---
+++ Assets/PlantCatalog.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantCatalog : MonoBehaviour
+{
+
+    public GameObject[] types;
+    public int NumberOfObjects;
+    public int ChoosenType = 0;
+
+    void ChangeChoice()
+    {
+        for (int i=0; i<NumberOfObjects; i++)
+        {
+            if (ChoosenType == i)
+            {
+                types[i].SetActive(true);
+            }
+            else
+            {
+                types[i].SetActive(false);
+            }
+        }
+    }
+
+    public void add()
+    {
+        ChoosenType += 1;
+        if (ChoosenType == 5)
+        {
+            ChoosenType = 0;
+        }
+        ChangeChoice();
+    }
+
+    public void reduce()
+    {
+        ChoosenType -= 1;
+        if (ChoosenType < 0)
+        {
+            ChoosenType = 4;
+        }
+        ChangeChoice();
+    }
+
+
+
+}
---
+++ Assets/scripts/scene 4/PlanetType.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetType : MonoBehaviour
+{
+    public string TypeName;
+    public string Size;
+    public string Water, Light, Life, Atmosphere, Temprature, Gravity;
+    public string Surface;
+    public TextMeshProUGUI N, Sz, W, Lg, Lf, A, T, G, Sf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        N.text = TypeName;
+        Sz.text = Size;
+        W.text = Water + "%";
+        Lg.text = Light + "%";
+        Lf.text = Life;
+        A.text = Atmosphere + "%";
+        T.text = Temprature + "c";
+        G.text = Gravity;
+        Sf.text = Surface;
+    }
+}
