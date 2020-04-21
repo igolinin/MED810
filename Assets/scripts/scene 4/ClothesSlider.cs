b942ea97ce381a5de12a2d563c5e5edbb36498bb
Yanivg2906 <55092334+Yanivg2906@users.noreply.github.com>
2020-04-21 19:49:11 +0200

    added bouncing ball mechanisem, outfit sliders

---
+++ Assets/scripts/scene 4/ClothesSlider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClothesSlider : MonoBehaviour
+{
+    public GameObject[] Characters;
+    public float Current;
+    public Slider slider;
+    public int max;
+
+    // Start is called before the first frame update
+
+
+    public void chracterSwitch()
+    {
+        Current = slider.value;
+        for (int i=0; i<max; i++)
+        {
+            if (i==Current)
+            {
+                Characters[i].SetActive(true);
+            }
+            else
+            {
+                Characters[i].SetActive(false);
+            }
+        }
+    }
+
+}
