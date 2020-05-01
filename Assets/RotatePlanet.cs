cdfe20745ede0b8fe756f994e005d0bfde5632a9
aleflavia <alejandra.mesa10@gmail.com>
2020-05-01 21:29:47 +0200

    8 planets assets
    Planets and background asset
    It includes a script to make them rotate
    Missing Earth-like exoplanet and Super Earth

---
+++ Assets/RotatePlanet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + speed, transform.rotation.eulerAngles.z));
+    }
+}
