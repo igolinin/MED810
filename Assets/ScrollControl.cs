fb1ac265803fb67470ab4b3ecc2e5ecad754e76c
Yanivg2906 <55092334+Yanivg2906@users.noreply.github.com>
2020-05-06 22:57:21 +0200

    added scroll control
    still working on anchors

---
+++ Assets/ScrollControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollControl : MonoBehaviour
+{
+    public Vector3 PlaceHolder;
+    public GameObject[] types;
+    private bool isDrag= false;
+    private Vector3[] positions;
+    private int closestN;
+
+
+
+    private void Start()
+    {
+        PlaceHolder = types[0].transform.position;
+
+    }
+}
