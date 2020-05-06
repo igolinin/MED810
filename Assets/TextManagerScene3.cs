1c1a0a91692331c9d00b30d2a8cd5310fffa01ab
Emil Berzén <emilberzen@gmail.com>
2020-05-06 10:42:00 +0200

    Fixes on station 3

---
+++ Assets/TextManagerScene3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManagerScene3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < CameraController2.SelectedPlanet.Count; i++)
+        {
+            if (CameraController2.SelectedPlanet[i] == 0)
+            {
+
+
+            }
+
+
+        }
+      
+    }
+}
--- Assets/scripts/S3toS4.cs
+++ Assets/scripts/S3toS4.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class S3toS4 : MonoBehaviour
 {
 
-   public static bool[] ListFound = new bool[10];
+    public static bool[] ListFound = new bool[10];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(CameraController2.SelectedPlanet.Count + " What I FOUND!!!");
-    }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
         for (int i = 0; i < ListFound.Length; i++)
         {
             for (int j = 0; j < CameraController2.SelectedPlanet.Count; j++)
@@ -28,5 +26,7 @@ public class S3toS4 : MonoBehaviour
             }
 
         }
+
+
     }
 }
