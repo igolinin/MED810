81d64a6a670c5f67821f06dd4c024542e69758ed
Yanivg2906 <55092334+Yanivg2906@users.noreply.github.com>
2020-05-10 22:08:04 +0200

    Surface works, Text work in progress, improved filters

--- Assets/scripts/scene 4/PlanetRepresentation.cs
+++ Assets/scripts/scene 4/PlanetRepresentation.cs
@@ -6,12 +6,8 @@ public class PlanetRepresentation : MonoBehaviour
 {
     public Material[] Materials;
     public GameObject Control;
-    public PhysicMaterial[] physicmatierals;
-    public Material[] SurMaterials;
-    public bool[] trigger;
     public int Num=0;
     int i;
-    public bool IsSurface;
     public Color[] colors;
 
     // Start is called before the first frame update
@@ -25,14 +21,7 @@ public class PlanetRepresentation : MonoBehaviour
     {
         if ((Num+1) != Control.GetComponent<ScreenControl>().PlanetN && Control.GetComponent<ScreenControl>().PlanetN!=0)
         {
-            if (IsSurface)
-            {
-                ChangeSurface();
-            }
-            else
-            {
                 ChangeSpheres();
-            }
 
         }
         
@@ -45,38 +34,6 @@ public class PlanetRepresentation : MonoBehaviour
         gameObject.GetComponent<Renderer>().material = Materials[Num];
     }
 
-    void ChangeSurface()
-    {
-        if (Num == 2 || Num == 3 || Num == 7 || Num == 8)
-        {
-            //Gas planets
-            i = 0;
-        }
-        else if (Num == 1 || Num == 4)
-        {
-            //Ice planets
-            i = 1;
-        }
-        else if (Num==5)
-        {
-            //Lava
-            i = 2;
-        }
-        else if(Num == 9)
-        {
-            //water
-            i = 3;
-        }
-        else
-        { //earth
-            i = 4;
-        }
-
-        BoxCollider a = gameObject.GetComponent<BoxCollider>();
-        a.material = physicmatierals[i];
-        a.isTrigger = trigger[i];
-        gameObject.GetComponent<MeshRenderer>().material = SurMaterials[i];
-    }
 
     public void Fil(int a)
     {
---
+++ Assets/scripts/scene 4/ThrowBall.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowBall : MonoBehaviour
+{
+    public GameObject[] Surfaces;
+    public GameObject ball;
+    int Num;
+    int i;
+    public ScreenControl sc;
+    public float[] Grav;
+    public GameObject ball1;
+    public Slider slider;
+    Vector3 ballPosition;
+    Vector3 ball1Position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballPosition = ball.transform.position;
+        ball1Position = ball1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Num != sc.PlanetN&& sc.PlanetN!=0)
+        {
+            Num = sc.PlanetN;
+            ChangeSurface();
+        }
+    }
+
+    void ChangeSurface()
+    {
+        if (Num == 2 || Num == 3 || Num == 7 || Num == 8)
+        {
+            //Gas planets
+            i = 0;
+        }
+        else if (Num == 1 || Num == 4)
+        {
+            //Ice planets
+            i = 1;
+        }
+        else if (Num == 5)
+        {
+            //Lava
+            i = 2;
+        }
+        else if (Num == 9)
+        {
+            //water
+            i = 3;
+        }
+        else
+        { //earth
+            i = 4;
+        }
+        Surfaces[i].SetActive(true);
+        ball.GetComponent<Rigidbody>().mass = Grav[i];
+    }
+
+    public void Restart()
+    {
+        slider.value = 0.0f;
+        Rigidbody r = ball.GetComponent<Rigidbody>();
+        Rigidbody r2 = ball1.GetComponent<Rigidbody>();
+        r.useGravity = false;
+        r2.useGravity = false;
+        r.velocity = new Vector3(0,0);
+        r2.velocity = new Vector3(0, 0);
+        ball.transform.position = ballPosition;
+        ball1.transform.position = ball1Position;
+
+    }
+}
